c6e6ea8a34a1b7bf40d29d75f7c7cfc8e5c913e6
s22150 <s22150@pjwstk.edu.pl>
2023-03-12 18:59:35 +0100

    zrobione zadanie

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+
 import webbrowser
+
+import requests
+
 if __name__ == '__main__':
-    print("hello world")
-    url = input("Podaj stronę internetową: ")
-    webbrowser.open(url)
+    urlPage = input("Podaj stronę internetową: ")
+    A = [20100101,20150202,20200303]
+    for number in range(len(A)):
+        url = "http://archive.org/wayback/available?url=" + urlPage + "&timestamp=" + str(A[number])
+        response = requests.get(url)
+        d = response.json()
+        page = d["archived_snapshots"]["closest"]["url"]
+        webbrowser.open(page)
